c37a2df6b713423881ef7347abcdfaacc1afd99f
CFerrell <cferrell@rogers.com>
2017-07-02 15:34:33 -0400

    404 page

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The 404 page
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
+ */
+
+get_header(); ?>
+
+<div class="oops">
+<h2> Oops.. we can't find that page! </h2>
+</div>
+
+<div class="lost">
+
+</div>
